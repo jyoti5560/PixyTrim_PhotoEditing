0612fda8f2254c2758f25a7ec8166e4fb5d70ccc
jyoti <90950176+jyoti5560@users.noreply.github.com>
2021-12-08 16:05:09 +0530

    crop image and rotate image

--- pixytrim/lib/screens/crop_image_screen/crop_image_screen.dart
+++ pixytrim/lib/screens/crop_image_screen/crop_image_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 import 'dart:math' as Math;
-import 'package:crop_your_image/crop_your_image.dart';
+//import 'package:crop_your_image/crop_your_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -10,10 +10,15 @@ import 'package:get/get.dart';
 //import 'package:image_crop/image_crop.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:pixytrim/common/common_widgets.dart';
+import 'package:pixytrim/common/custom_color.dart';
+import 'package:pixytrim/common/custom_gradient_slider.dart';
 import 'package:pixytrim/common/custom_image.dart';
 import 'dart:ui' as ui;
 import 'dart:math' as math;
+import 'package:crop/crop.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 //import 'package: photo_view/photo_view.dart';
 
 class CropImageScreen extends StatefulWidget {
@@ -32,22 +37,24 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
   File? file;
   File? file2;
   final GlobalKey key = GlobalKey();
-  final _cropController = CropController();
+  //final _cropController = CropController();
   Uint8List? _croppedData;
   var _isCropping = false;
 
   int index = 0;
   bool showFront = true;
-  AnimationController ? controller;
+  double _rotation = 0;
+  final controller = CropController(aspectRatio: 1000 / 667.0);
+  BoxShape shape = BoxShape.rectangle;
 
-  @override
-  void initState() {
-    super.initState();
 
-    // Initialize the animation controller
-    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 300), value: 0);
-  }
-
+  LinearGradient gradient = LinearGradient(
+      colors: <Color> [
+        AppColor.kBorderGradientColor1,
+        AppColor.kBorderGradientColor2,
+        AppColor.kBorderGradientColor3,
+      ]
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -72,41 +79,72 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
                       width: Get.width,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(20),
-                        child:  _croppedData != null ?
-                        RepaintBoundary(
-                          key: key,
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child:  Image.memory(_croppedData!)),
-                        ):
-                        Crop(
-                          controller: _cropController,
-                          image: widget.file.readAsBytesSync(),
-                          onCropped: (croppedData) {
-                            setState(() {
-                              _croppedData = croppedData;
-                              _isCropping = false;
-                            });
-                          },
-                          //withCircleUi: _isCircleUi,
-                          // onStatusChanged: (status) => setState(() {
-                          //   _statusText = <CropStatus, String>{
-                          //     CropStatus.nothing: 'Crop has no image data',
-                          //     CropStatus.loading:
-                          //     'Crop is now loading given image',
-                          //     CropStatus.ready: 'Crop is now ready!',
-                          //     CropStatus.cropping:
-                          //     'Crop is now cropping image',
-                          //   }[status] ??
-                          //       '';
-                          // }),
-                          initialSize: 0.5,
-                         // maskColor: _isSumbnail ? Colors.white : null,
-                         //  cornerDotBuilder: (size, edgeAlignment) => _isSumbnail
-                         //      ? const SizedBox.shrink()
-                         //      : const DotControl(),
-                        ),
+                        // child:  _croppedData != null ?
+                        // RepaintBoundary(
+                        //   key: key,
+                        //   child: ClipRRect(
+                        //       borderRadius: BorderRadius.circular(20),
+                        //       child:  Image.memory(_croppedData!)),
+                        // ):
+                        // Crop(
+                        //   controller: _cropController,
+                        //   image: widget.file.readAsBytesSync(),
+                        //   onCropped: (croppedData) {
+                        //     setState(() {
+                        //       _croppedData = croppedData;
+                        //       _isCropping = false;
+                        //     });
+                        //   },
+                        //   //withCircleUi: _isCircleUi,
+                        //   // onStatusChanged: (status) => setState(() {
+                        //   //   _statusText = <CropStatus, String>{
+                        //   //     CropStatus.nothing: 'Crop has no image data',
+                        //   //     CropStatus.loading:
+                        //   //     'Crop is now loading given image',
+                        //   //     CropStatus.ready: 'Crop is now ready!',
+                        //   //     CropStatus.cropping:
+                        //   //     'Crop is now cropping image',
+                        //   //   }[status] ??
+                        //   //       '';
+                        //   // }),
+                        //   initialSize: 0.5,
+                        //  // maskColor: _isSumbnail ? Colors.white : null,
+                        //  //  cornerDotBuilder: (size, edgeAlignment) => _isSumbnail
+                        //  //      ? const SizedBox.shrink()
+                        //  //      : const DotControl(),
+                        // ),
 
+                        child: Crop(
+                          onChanged: (decomposition) {
+                            if (_rotation != decomposition.rotation) {
+                              setState(() {
+                                _rotation = ((decomposition.rotation + 180) % 360) - 180;
+                              });
+                            }
+
+                            print(
+                                "Scale : ${decomposition.scale}, Rotation: ${decomposition.rotation}, translation: ${decomposition.translation}");
+                          },
+                          controller: controller,
+                          shape: shape,
+                          child: Image.file(widget.file),
+                          foreground: IgnorePointer(
+                            child: Container(
+                              alignment: Alignment.bottomRight,
+                              child: Text(
+                                'Foreground Object',
+                                style: TextStyle(color: Colors.red),
+                              ),
+                            ),
+                          ),
+                          helper: shape == BoxShape.rectangle
+                              ? Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white, width: 2),
+                            ),
+                          )
+                              : null,
+                        ),
                       ),
                     )
                 ),
@@ -132,29 +170,68 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
       ),
     );
   }
-  double sat = 1;
+
   Widget rotateRatio(){
-    return Slider(
-      //label: 'sat : ${sat.toStringAsFixed(2)}',
-      onChanged: (double value) {
-        setState(() {
-          sat = value;
-        });
-      },
-      divisions: 50,
-      value: sat,
-      min: 0,
-      max: 2,
+    return Container(
+      padding: EdgeInsets.all(2),
+      decoration: borderGradientDecoration(),
+      child: Container(
+        padding: EdgeInsets.only(left: 10, right: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
+        child: SliderTheme(
+          data: SliderThemeData(
+            trackShape: GradientRectSliderTrackShape(gradient: gradient, darkenInactive: false),
+          ),
+          child: Slider(
+            divisions: 360,
+            value: _rotation,
+            min: -180,
+            max: 180,
+            label: '$_rotation°',
+            onChanged: (n) {
+              setState(() {
+                _rotation = n.roundToDouble();
+                controller.rotation = _rotation;
+              });
+            },
+          ),
+        ),
+      ),
     );
   }
 
   Widget scaleRatio(){
-    return Slider(
-      activeColor: Colors.green,
-      onChanged: (value){
-
-      },
-      value: 0,
+    return Container(
+      padding: EdgeInsets.all(2),
+      decoration: borderGradientDecoration(),
+      child: Container(
+        padding: EdgeInsets.only(left: 10, right: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
+        child: SliderTheme(
+          data: SliderThemeData(
+            trackShape: GradientRectSliderTrackShape(gradient: gradient, darkenInactive: false),
+          ),
+          child: Slider(
+            divisions: 360,
+            value: _rotation,
+            min: -180,
+            max: 180,
+            label: '$_rotation°',
+            onChanged: (n) {
+              setState(() {
+                _rotation = n.roundToDouble();
+                controller.rotation = _rotation;
+              });
+            },
+          ),
+        ),
+      ),
     );
   }
 
@@ -189,14 +266,18 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
                   ),
                 ),
                 GestureDetector(
-                  onTap: ()async {
+                  onTap: () {
                     //Get.back();
                     //await _capturePng();
-                    setState(() {
-                      _isCropping = true;
-                    });
-                    _cropController.crop();
-                    _capturePng();
+
+
+                    // setState(() {
+                    //   _isCropping = true;
+                    // });
+                    // _cropController.crop();
+                    // _capturePng();
+
+                     crop();
                   },
                   child: Container(
                       child: Icon(Icons.check_rounded)
@@ -206,6 +287,30 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
             )),
       ),
     );
+  }
+
+  void crop() async {
+    final pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    final cropped = await controller.crop(pixelRatio: pixelRatio);
+
+    final status = await Permission.storage.request();
+    if (status == PermissionStatus.granted) {
+      await _saveScreenShot(cropped);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Saved to gallery.'),
+        ),
+      );
+    }
+  }
+
+  Future<dynamic> _saveScreenShot(ui.Image img) async {
+    var byteData = await img.toByteData(format: ui.ImageByteFormat.png);
+    var buffer = byteData!.buffer.asUint8List();
+    final result = await ImageGallerySaver.saveImage(buffer);
+    print(result);
+
+    return result;
   }
 
   Future _capturePng() async {
@@ -255,7 +360,8 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
         children: [
           GestureDetector(
             onTap: (){
-              _cropController.aspectRatio = 1/1;
+                controller.aspectRatio = 1;
+              //_cropController.aspectRatio = 1/1;
             },
             child: Container(
               child: Text("1:1",
@@ -264,7 +370,8 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
           ),
           GestureDetector(
             onTap: (){
-              _cropController.aspectRatio = 3/2;
+              controller.aspectRatio = 3.0 / 2.0;
+             // _cropController.aspectRatio = 3/2;
             },
             child: Container(
                 child: Text("3:2", style: TextStyle(fontSize: 18, fontFamily: ""),)
@@ -272,7 +379,8 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
           ),
           GestureDetector(
             onTap: (){
-              _cropController.aspectRatio = 1/1;
+              //_cropController.aspectRatio = 1/1;
+              controller.aspectRatio = 1000 / 667.0;
             },
             child: Container(
                 child: Text("Original", style: TextStyle(fontSize: 18, fontFamily: ""),)
@@ -280,7 +388,8 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
           ),
           GestureDetector(
             onTap: (){
-              _cropController.aspectRatio = 4 / 3;
+              controller.aspectRatio = 4.0 / 3.0;
+              //_cropController.aspectRatio = 4 / 3;
             },
             child: Container(
                 child: Text("4:3", style: TextStyle(fontSize: 18, fontFamily: ""),)
@@ -289,7 +398,8 @@ class _CropImageScreenState extends State<CropImageScreen> with SingleTickerProv
           GestureDetector(
             onTap: (){
               //_isCircleUi = false;
-              _cropController.aspectRatio = 16 / 9;
+              controller.aspectRatio = 16.0 / 9.0;
+              //_cropController.aspectRatio = 16 / 9;
             },
             child: Container(
                 child: Text("16:9", style: TextStyle(fontSize: 18, fontFamily: ""),)
